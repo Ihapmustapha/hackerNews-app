c4f370feceeac4244a17f5239bbe8051b4a37e7e
ihapmustapha <ihapmustapha@gmail.com>
2019-11-04 19:17:26 +0200

    mapping multiple stories in StoriesContainer

--- src/containers/storiesContainer.js
+++ src/containers/storiesContainer.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { getStoryIds } from "../services/hackerNewsApi";
+import Story from "../components/Story";
 
 const StoriesContainer = () => {
   const [storyIds, setStoryIds] = useState([]);
 
   useEffect(() => {
     getStoryIds()
-      .then(({ data }) => data && setStoryIds(data))
+      .then(data => data && setStoryIds(data))
       .catch(error => console.error(error));
-  }, []);
+  });
 
-  return <p>{JSON.stringify(storyIds)}</p>;
+  return storyIds.map(storyId => <Story id={storyId} key={`key-` + storyId} />);
 };
 
 export default StoriesContainer;
