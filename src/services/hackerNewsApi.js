ec4b6704f7a1a0b082456d56b81f00167267dc09
ihapmustapha <ihapmustapha@gmail.com>
2019-11-04 19:17:44 +0200

    adding getStory service

--- src/services/hackerNewsApi.js
+++ src/services/hackerNewsApi.js
@@ -4,4 +4,8 @@ export const baseUrl = "https://hacker-news.firebaseio.com/v0/";
 export const newStoriesUrl = `${baseUrl}newstories.json`;
 export const storyUrl = `${baseUrl}item/`;
 
-export const getStoryIds = async () => await Axios.get(newStoriesUrl).then(stories => stories);
+export const getStory = async storyId =>
+  await Axios.get(storyUrl + storyId + `.json`).then(({ data }) => data);
+
+export const getStoryIds = async () =>
+  await Axios.get(newStoriesUrl).then(({ data }) => data);
