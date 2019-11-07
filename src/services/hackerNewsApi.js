bf9f86ed24974f396cbe91f5ae1a89589d5a58fa
ihapmustapha <ihapmustapha@gmail.com>
2019-11-07 22:10:36 +0200

    Adding selectFields method

--- src/services/hackerNewsApi.js
+++ src/services/hackerNewsApi.js
@@ -1,11 +1,14 @@
 import Axios from "axios";
+import { selectFields } from "../utils/selectFields";
 
 export const baseUrl = "https://hacker-news.firebaseio.com/v0/";
 export const newStoriesUrl = `${baseUrl}newstories.json`;
 export const storyUrl = `${baseUrl}item/`;
 
 export const getStory = async storyId =>
-  await Axios.get(storyUrl + storyId + `.json`).then(({ data }) => data);
+  await Axios.get(storyUrl + storyId + `.json`).then(
+    ({ data }) => data && selectFields(data)
+  );
 
 export const getStoryIds = async () =>
   await Axios.get(newStoriesUrl).then(({ data }) => data);
